1b1a0c30c8fc99ca4e37aa9884601f9d49e065fb
mjairumi <mohammadjairumi43@gmail.com>
2022-05-28 21:57:10 +0530

    Setup MySQL database, make post API request, Handle Post API request

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { useState } from "react";
+import Axios from "axios";
 
 function App() {
   const [emInfo, setEm] = useState({
@@ -10,32 +11,59 @@ function App() {
     wage: 0,
   });
 
-  function handleChange(event){
-    const {name, value} = event.target;
-    setEm(prev => {
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setEm((prev) => {
       return {
         ...prev,
-        [name]: value
-      }
-    })
+        [name]: value,
+      };
+    });
   }
 
-  function handleOnClick (){
-    console.log(emInfo)
+  function handleOnClick() {
+    Axios.post("http://localhost:3001/create", { emInfo: emInfo }).then(() => {
+      console.log("Info Submitted");
+    });
   }
   return (
     <div className="App">
       <div className="info">
         <label>Name:</label>
-        <input type="text" name="name" value={emInfo.name} onChange={handleChange}/>
+        <input
+          type="text"
+          name="name"
+          value={emInfo.name}
+          onChange={handleChange}
+        />
         <label>Age:</label>
-        <input type="number" name="age" value={emInfo.age} onChange={handleChange}/>
+        <input
+          type="number"
+          name="age"
+          value={emInfo.age}
+          onChange={handleChange}
+        />
         <label>Country:</label>
-        <input type="text" name="country" value={emInfo.country} onChange={handleChange}/>
+        <input
+          type="text"
+          name="country"
+          value={emInfo.country}
+          onChange={handleChange}
+        />
         <label>Position:</label>
-        <input type="text" name="position" value={emInfo.position} onChange={handleChange}/>
+        <input
+          type="text"
+          name="position"
+          value={emInfo.position}
+          onChange={handleChange}
+        />
         <label>Wage (year):</label>
-        <input type="text" name="wage" value={emInfo.wage} onChange={handleChange}/>
+        <input
+          type="text"
+          name="wage"
+          value={emInfo.wage}
+          onChange={handleChange}
+        />
         <button onClick={handleOnClick}>Add Employeer</button>
       </div>
     </div>
---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+  user: "root",
+  host: "localhost",
+  password: "MyCourses@43",
+  database: "employeesystem",
+});
+
+app.post("/create", (req, res) => {
+  const emInfo = req.body.emInfo;
+
+  db.query(
+    "INSERT INTO employees (name, age, country, position, wage) VALUES (?, ?, ?, ?, ?)",
+    [emInfo.name, emInfo.age, emInfo.country, emInfo.position, emInfo.wage],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("Inserted Entry");
+      }
+    }
+  );
+});
+
+app.listen(3001, () => {
+  console.log("Sever is running at port 3001");
+});
